967d281221870350335f4802ad303b97f3a8e5cb
kokol12 <kokoleismail886@gmail.com>
2023-04-27 22:28:23 +0300

    task json output to print out objects of each instance model

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,9 +5,22 @@ flask module
 """
 from flask import Flask, jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     """ Returning the json string"""
-    return jsonify({"status": "Ok"})
+    return jsonify({"status": "OK"})
+
+
+@app_views.route('/api/v1/stats', strict_slashes=False)
+def count_objects():
+    """Couniting objects of each instance"""
+    return jsonify({"amenities": storage.count('Amenity'),
+                    "cities": storage.count('City'),
+                    "places": storage.count('Place'),
+                    "reviews": storage.count('Review'),
+                    "states": storage.count('State'),
+                    "users": storage.count('User')})
+
