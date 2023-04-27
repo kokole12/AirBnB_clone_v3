d7217a94901b620d5664f54542643d53ebf55077
kokol12 <kokoleismail886@gmail.com>
2023-04-28 00:07:52 +0300

    created get method for all states, get method for a single state, a delete method to delete a state by the id, and to create the post method to create a new states

---
+++ api/v1/views/states.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" Python flask view for managing the state resource"""
+
+from flask import Flask, jsonify, abort, request
+from api.v1.views import app_views
+from models import storage
+from models.state import State
+
+
+@app_views.route("/api/v1/states", methods=['GET'], strict_slashes=False)
+def get_states():
+    """Returning all the states"""
+    states = storage.all(State).values()
+    states_dict = [state.to_dict() for state in states]
+    return jsonify(states_dict)
+
+
+@app_views.route("/api/v1/states/<state_id>", methods=['GET'], strict_slashes=False)
+def get_state(state_id):
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    return jsonify(state.to_dict())
+
+
+@app_views.route("/api/v1/states/<state_id>", methods=['DELETE'], strict_slashes=False)
+def delete_state(state_id):
+    """deletes the state from the database"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    state.delete()
+    storage.save()
+    return jsonify({})
+
+
+@app_views.route("/api/v1/states", methods=['POST'], strict_slashes=False)
+def create_state():
+    if not request.get_json():
+        pass
