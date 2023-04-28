db8376393bcb97720efd06e3e9f342862e72f8f2
kokol12 <kokoleismail886@gmail.com>
2023-04-28 11:10:03 +0300

    created the cities view and created the get method to get cities from a given state

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -7,7 +7,7 @@ from models import storage
 from models.state import State
 
 
-@app_views.route("/api/v1/states", methods=['GET'],
+@app_views.route("/states", methods=['GET'],
                  strict_slashes=False)
 def get_states():
     """Returning all the states"""
@@ -16,7 +16,7 @@ def get_states():
     return jsonify(states_dict)
 
 
-@app_views.route("/api/v1/states/<state_id>", methods=['GET'],
+@app_views.route("/states/<state_id>", methods=['GET'],
                  strict_slashes=False)
 def get_state(state_id):
     state = storage.get(State, state_id)
@@ -25,7 +25,7 @@ def get_state(state_id):
     return jsonify(state.to_dict())
 
 
-@app_views.route("/api/v1/states/<state_id>", methods=['DELETE'],
+@app_views.route("/states/<state_id>", methods=['DELETE'],
                  strict_slashes=False)
 def delete_state(state_id):
     """deletes the state from the database"""
@@ -37,7 +37,7 @@ def delete_state(state_id):
     return jsonify({})
 
 
-@app_views.route("/api/v1/states", methods=['POST'],
+@app_views.route("/states", methods=['POST'],
                  strict_slashes=False)
 def create_state():
     if not request.get_json():
@@ -51,7 +51,7 @@ def create_state():
     return jsonify(new_state.to_dict()), 201
 
 
-@app_views.route('/api/v1/states/<state_id>', methods=['PUT'],
+@app_views.route('/states/<state_id>', methods=['PUT'],
                  strict_slashes=False)
 def update_state(state_id):
     state = storage.get(State, state_id)
