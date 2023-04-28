cdb12320e2cf3faa885c86c5b18e81daa0aca116
kokol12 <kokoleismail886@gmail.com>
2023-04-28 16:55:22 +0300

    crud operations for place to be implemented

---
+++ api/v1/views/places.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+python flask script to create crud
+operations for places
+"""
+
+
+from flask import Flask, json, abort, make_response, request
+from models import storage
+from models.place import Place
+
+
+"""TO DO implement the CRUD operations for place"""
