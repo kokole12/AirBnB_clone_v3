92fafae452ebf26194ce5e1b949bf0ebda991e06
kokol12 <kokoleismail886@gmail.com>
2023-04-28 10:29:36 +0300

    codes formated to pep8 style and file states.py is made executable

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -7,7 +7,8 @@ from models import storage
 from models.state import State
 
 
-@app_views.route("/api/v1/states", methods=['GET'], strict_slashes=False)
+@app_views.route("/api/v1/states", methods=['GET'],
+                 strict_slashes=False)
 def get_states():
     """Returning all the states"""
     states = storage.all(State).values()
@@ -15,7 +16,8 @@ def get_states():
     return jsonify(states_dict)
 
 
-@app_views.route("/api/v1/states/<state_id>", methods=['GET'], strict_slashes=False)
+@app_views.route("/api/v1/states/<state_id>", methods=['GET'],
+                 strict_slashes=False)
 def get_state(state_id):
     state = storage.get(State, state_id)
     if state is None:
@@ -23,7 +25,8 @@ def get_state(state_id):
     return jsonify(state.to_dict())
 
 
-@app_views.route("/api/v1/states/<state_id>", methods=['DELETE'], strict_slashes=False)
+@app_views.route("/api/v1/states/<state_id>", methods=['DELETE'],
+                 strict_slashes=False)
 def delete_state(state_id):
     """deletes the state from the database"""
     state = storage.get(State, state_id)
@@ -34,7 +37,8 @@ def delete_state(state_id):
     return jsonify({})
 
 
-@app_views.route("/api/v1/states", methods=['POST'], strict_slashes=False)
+@app_views.route("/api/v1/states", methods=['POST'],
+                 strict_slashes=False)
 def create_state():
     if not request.get_json():
         """ TO DO --write logic to create a new state"""
@@ -47,9 +51,10 @@ def create_state():
     return (jsonify(new_state.to_dict()), 201)
 
 
-@app_views.route('/api/v1/states/<state_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/api/v1/states/<state_id>', methods=['PUT'],
+                 strict_slashes=False)
 def update_state(state_id):
-    state =  storage.get(State, state_id)
+    state = storage.get(State, state_id)
     if state is None:
         abort(404)
     if not request.get_json():
