e40adede3b857f969c79e0242b2aa484c40a48c0
kokol12 <kokoleismail886@gmail.com>
2023-04-28 23:36:29 +0300

    updated file

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -20,10 +20,10 @@ def get_users():
 def get_user(user_id):
     """Getting user by the Id"""
     user = storage.get(User, user_id)
-    if user is None:
-        abort(404)
-    else:
+    if user is not None:
         return jsonify(user.to_dict())
+    else:
+        abort(404)
 
 
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
