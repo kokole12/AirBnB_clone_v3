4eb3663f6f463d2d0f7f8e94664b7f2be6d0604f
kokol12 <kokoleismail886@gmail.com>
2023-04-29 21:57:25 +0300

    rewrote the put method in cities

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -11,8 +11,9 @@ from api.v1.views import app_views
 @app_views.route('/amenities', methods=['GET'], strict_slashes=False)
 def get_amenities():
     """getting the amenities from the database"""
-    amenities = storage.all(Amenity).values()
-    amenities_dict = [amenity.to_dict() for amenity in amenities]
+    
+    amenities_dict = [amenity.to_dict() for amenity in storage.all(Amenity).
+                      values()]
     return jsonify(amenities_dict)
 
 
