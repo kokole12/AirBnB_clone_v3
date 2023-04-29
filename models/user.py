e74a0fa263c0e1bac0a418721e8399ea1a56eeec
kokol12 <kokoleismail886@gmail.com>
2023-04-29 22:30:23 +0300

    implementing the password hashing using md5

--- models/user.py
+++ models/user.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """ holds class User"""
+from typing import Any
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from hashlib import md5
 
 
 class User(BaseModel, Base):
@@ -27,3 +29,8 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+
+    def __setattr__(self, name, value) -> None:
+        if name == "password":
+            value = md5(value.encode()).hexdigest()
+        return super().__setattr__(name, value)
