be539968dbd31e96d8b178928bb59b14e3973307
kokol12 <kokoleismail886@gmail.com>
2023-04-29 23:49:22 +0300

    edited files:

--- models/user.py
+++ models/user.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """ holds class User"""
-from typing import Any
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from hashlib import md5
 
 
 class User(BaseModel, Base):
@@ -29,10 +27,3 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
-
-    """def __setattr__(self, name, value):
-        setting new hashed value for password
-        if name == "password":
-            value = md5(value.encode()).hexdigest()
-        return super().__setattr__(name, value)
-    """
